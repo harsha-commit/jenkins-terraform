4d982af609842dacf66e279ed0b53dd2b7c1e705
Harsha Vardhan Bashavathini <118506678+harsha-commit@users.noreply.github.com>
2024-06-23 15:03:34 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   cidr   = "10.0.0.0/16"
 
-  azs             = ["us-west-1a", "us-west-1b"]
+  azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.11.0/24", "10.0.12.0/24"]
 
